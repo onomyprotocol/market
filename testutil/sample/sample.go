56f6383875c687e8176851ca489f53d89aa76fa9
karthikonomy <karthikp1723@gmail.com>
2023-03-31 20:55:42 -0500

    added pool already exists testcase.

--- testutil/sample/sample.go
+++ testutil/sample/sample.go
@@ -11,3 +11,26 @@ func AccAddress() string {
 	addr := pk.Address()
 	return sdk.AccAddress(addr).String()
 }
+
+// SampleCoins returns the required NewCoins
+func SampleCoins(coina string, coinb string) sdk.Coins {
+
+	coinA, err := sdk.ParseCoinNormalized(coina)
+	if err != nil {
+		panic(err)
+	}
+
+	coinB, err := sdk.ParseCoinNormalized(coinb)
+	if err != nil {
+		panic(err)
+	}
+
+	return sdk.NewCoins(coinA, coinB)
+}
+
+// SampleDenoms returns the required denoms values
+func SampleDenoms(coins sdk.Coins) (denomA string, denomB string) {
+	denom1 := coins.GetDenomByIndex(0)
+	denom2 := coins.GetDenomByIndex(1)
+	return denom1, denom2
+}
--- x/market/keeper/msg_server_create_pool_test.go
+++ x/market/keeper/msg_server_create_pool_test.go
@@ -13,27 +13,33 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+type testData struct {
+	coinAStr      string
+	coinBStr      string
+	RateAstrArray []string
+	RateBstrArray []string
+}
+
 var addr string = sample.AccAddress()
 
 func TestCreatePool(t *testing.T) {
 	testInput := keepertest.CreateTestEnvironment(t)
-	// CoinAmsg and CoinBmsg pre-sort from raw msg
-	coinA, err := sdk.ParseCoinNormalized("20CoinA")
-	if err != nil {
-		require.Error(t, err)
-	}
+	//TestData
+	testdata := testData{coinAStr: "20CoinA", coinBStr: "20CoinB", RateAstrArray: []string{"10", "20"}, RateBstrArray: []string{"20", "30"}}
+	coinPair := sample.SampleCoins(testdata.coinAStr, testdata.coinBStr)
+	denomA, denomB := sample.SampleDenoms(coinPair)
+	pair := strings.Join([]string{denomA, denomB}, ",")
 
-	coinB, err := sdk.ParseCoinNormalized("20CoinB")
-	if err != nil {
-		require.Error(t, err)
-	}
-
-	coinPair := sdk.NewCoins(coinA, coinB)
+	//MintCoins
 	require.NoError(t, testInput.BankKeeper.MintCoins(testInput.Context, types.ModuleName, coinPair))
-	requestAddress, _ := sdk.AccAddressFromBech32(addr)
+	//SendCoinsFromModuleToAccount
+	requestAddress, err := sdk.AccAddressFromBech32(addr)
+	require.NoError(t, err)
 	require.NoError(t, testInput.BankKeeper.SendCoinsFromModuleToAccount(testInput.Context, types.ModuleName, requestAddress, coinPair))
-	beforecount := testInput.MarketKeeper.GetUidCount(testInput.Context) // GetUidCount before CreatePool
-	var p = types.MsgCreatePool{CoinA: "20CoinA", CoinB: "20CoinB", Creator: addr, RateA: []string{"10", "20"}, RateB: []string{"20", "30"}}
+	// GetUidCount before CreatePool
+	beforecount := testInput.MarketKeeper.GetUidCount(testInput.Context)
+	//Create Pool
+	var p = types.MsgCreatePool{CoinA: testdata.coinAStr, CoinB: testdata.coinBStr, Creator: addr, RateA: testdata.RateAstrArray, RateB: testdata.RateBstrArray}
 	response, err := keeper.NewMsgServerImpl(*testInput.MarketKeeper).CreatePool(sdk.WrapSDKContext(testInput.Context), &p)
 	//validate CreatePool
 	require.NoError(t, err)
@@ -44,20 +50,19 @@ func TestCreatePool(t *testing.T) {
 	aftercount := testInput.MarketKeeper.GetUidCount(testInput.Context)
 	require.Equal(t, beforecount+1, aftercount)
 	//validate GetPool
-	pair := strings.Join([]string{"CoinA", "CoinB"}, ",")
-	//pair1 := strings.Join([]string{"CoinB", "CoinA"}, ",")
+
 	rst, found := testInput.MarketKeeper.GetPool(testInput.Context, pair)
 	require.True(t, found)
 	require.Equal(t, rst.Pair, pair)
 	//validate GetMember
-	members, memberfound := testInput.MarketKeeper.GetMemberWithPair(testInput.Context, pair, "CoinB", "CoinA")
-	members1, memberfound1 := testInput.MarketKeeper.GetMemberWithPair(testInput.Context, pair, "CoinA", "CoinB")
+	members, memberfound := testInput.MarketKeeper.GetMemberWithPair(testInput.Context, pair, denomB, denomA)
+	members1, memberfound1 := testInput.MarketKeeper.GetMemberWithPair(testInput.Context, pair, denomA, denomB)
 	require.True(t, memberfound)
-	require.Equal(t, members.DenomA, "CoinB")
-	require.Equal(t, members.DenomB, "CoinA")
+	require.Equal(t, members.DenomA, denomB)
+	require.Equal(t, members.DenomB, denomA)
 	require.True(t, memberfound1)
-	require.Equal(t, members1.DenomA, "CoinA")
-	require.Equal(t, members1.DenomB, "CoinB")
+	require.Equal(t, members1.DenomA, denomA)
+	require.Equal(t, members1.DenomB, denomB)
 	//validate GetDrop
 	drops, dropFound := testInput.MarketKeeper.GetDrop(testInput.Context, aftercount)
 	require.True(t, dropFound)
@@ -65,6 +70,40 @@ func TestCreatePool(t *testing.T) {
 
 }
 
+func TestCreatePool_PoolAlreadyExist(t *testing.T) {
+	testInput := keepertest.CreateTestEnvironment(t)
+	count := 0
+	scenarios := []struct {
+		coinAStr      string
+		coinBStr      string
+		RateAstrArray []string
+		RateBstrArray []string
+	}{
+		{coinAStr: "20CoinA", coinBStr: "20CoinB", RateAstrArray: []string{"10", "20"}, RateBstrArray: []string{"20", "30"}},
+		{coinAStr: "20CoinA", coinBStr: "20CoinB", RateAstrArray: []string{"10", "20"}, RateBstrArray: []string{"20", "30"}},
+	}
+	for _, s := range scenarios {
+		coinPair := sample.SampleCoins("20CoinA", "20CoinB")
+
+		require.NoError(t, testInput.BankKeeper.MintCoins(testInput.Context, types.ModuleName, coinPair))
+		requestAddress, _ := sdk.AccAddressFromBech32(addr)
+		require.NoError(t, testInput.BankKeeper.SendCoinsFromModuleToAccount(testInput.Context, types.ModuleName, requestAddress, coinPair))
+		var p = types.MsgCreatePool{CoinA: s.coinAStr, CoinB: s.coinBStr, Creator: addr, RateA: s.RateAstrArray, RateB: s.RateBstrArray}
+		response, err := keeper.NewMsgServerImpl(*testInput.MarketKeeper).CreatePool(sdk.WrapSDKContext(testInput.Context), &p)
+		if count == 0 {
+			require.NoError(t, err)
+			require.Contains(t, p.GetCreator(), response.String())
+
+		} else {
+			require.Error(t, err) //Pool Already exists
+			require.NotContains(t, p.GetCreator(), response.String())
+		}
+
+		count++
+
+	}
+
+}
 func TestCreatePool_LessCoinPair(t *testing.T) {
 	testInput := keepertest.CreateTestEnvironment(t)
 
@@ -73,29 +112,21 @@ func TestCreatePool_LessCoinPair(t *testing.T) {
 		coinBStr      string
 		RateAstrArray []string
 		RateBstrArray []string
+		creater       string
 	}{
-		{coinAStr: "10CoinA", coinBStr: "20CoinB", RateAstrArray: []string{"10", "20"}, RateBstrArray: []string{"20", "30"}},
-		{coinAStr: "20CoinA", coinBStr: "10CoinB", RateAstrArray: []string{"10", "20"}, RateBstrArray: []string{"20", "30"}},
+		{coinAStr: "10CoinA", coinBStr: "20CoinB", RateAstrArray: []string{"10", "20"}, RateBstrArray: []string{"20", "30"}, creater: sample.AccAddress()},
+		{coinAStr: "20CoinA", coinBStr: "10CoinB", RateAstrArray: []string{"10", "20"}, RateBstrArray: []string{"20", "30"}, creater: sample.AccAddress()},
 	}
 	for _, s := range scenarios {
-		coinA, err := sdk.ParseCoinNormalized("20CoinA")
-		if err != nil {
-			require.Error(t, err)
-		}
+		coinPair := sample.SampleCoins("20CoinA", "20CoinB")
 
-		coinB, err := sdk.ParseCoinNormalized("20CoinB")
-		if err != nil {
-			require.Error(t, err)
-		}
-
-		coinPair := sdk.NewCoins(coinA, coinB)
 		require.NoError(t, testInput.BankKeeper.MintCoins(testInput.Context, types.ModuleName, coinPair))
 		requestAddress, _ := sdk.AccAddressFromBech32(addr)
 		require.NoError(t, testInput.BankKeeper.SendCoinsFromModuleToAccount(testInput.Context, types.ModuleName, requestAddress, coinPair))
-		var p = types.MsgCreatePool{CoinA: s.coinAStr, CoinB: s.coinBStr, Creator: addr, RateA: s.RateAstrArray, RateB: s.RateBstrArray}
+		var p = types.MsgCreatePool{CoinA: s.coinAStr, CoinB: s.coinBStr, Creator: s.creater, RateA: s.RateAstrArray, RateB: s.RateBstrArray}
 		response, err := keeper.NewMsgServerImpl(*testInput.MarketKeeper).CreatePool(sdk.WrapSDKContext(testInput.Context), &p)
-		require.NoError(t, err)
-		require.Contains(t, p.GetCreator(), response.String())
+		require.Error(t, err)
+		require.NotContains(t, p.GetCreator(), response.String())
 
 	}
 
@@ -103,21 +134,14 @@ func TestCreatePool_LessCoinPair(t *testing.T) {
 
 func TestCreatePool_Insufficient_Funds(t *testing.T) {
 	testInput := keepertest.CreateTestEnvironment(t)
-	coinA, err := sdk.ParseCoinNormalized("10CoinA")
-	if err != nil {
-		require.Error(t, err)
-	}
+	//TestData
+	testdata := testData{coinAStr: "15CoinA", coinBStr: "15CoinB", RateAstrArray: []string{"10", "20"}, RateBstrArray: []string{"20", "30"}}
+	coinPair := sample.SampleCoins("10CoinA", "10CoinB")
 
-	coinB, err := sdk.ParseCoinNormalized("10CoinB")
-	if err != nil {
-		require.Error(t, err)
-	}
-
-	coinPair := sdk.NewCoins(coinA, coinB)
 	require.NoError(t, testInput.BankKeeper.MintCoins(testInput.Context, types.ModuleName, coinPair))
 	requestAddress, _ := sdk.AccAddressFromBech32(addr)
 	require.NoError(t, testInput.BankKeeper.SendCoinsFromModuleToAccount(testInput.Context, types.ModuleName, requestAddress, coinPair))
-	var p = types.MsgCreatePool{CoinA: "15CoinA", CoinB: "15CoinB", Creator: addr, RateA: []string{"10", "20"}, RateB: []string{"20", "30"}}
+	var p = types.MsgCreatePool{CoinA: testdata.coinAStr, CoinB: testdata.coinBStr, Creator: addr, RateA: testdata.RateAstrArray, RateB: testdata.RateBstrArray}
 	response, err := keeper.NewMsgServerImpl(*testInput.MarketKeeper).CreatePool(sdk.WrapSDKContext(testInput.Context), &p)
 	require.Error(t, err)
 	require.NotContains(t, p.GetCreator(), response.String())
@@ -126,17 +150,8 @@ func TestCreatePool_Insufficient_Funds(t *testing.T) {
 
 func TestCreatePool_Insufficient_Funds_ReSubmit(t *testing.T) {
 	testInput := keepertest.CreateTestEnvironment(t)
-	coinA, err := sdk.ParseCoinNormalized("20CoinA")
-	if err != nil {
-		require.Error(t, err)
-	}
+	coinPair := sample.SampleCoins("20CoinA", "20CoinB")
 
-	coinB, err := sdk.ParseCoinNormalized("20CoinB")
-	if err != nil {
-		require.Error(t, err)
-	}
-
-	coinPair := sdk.NewCoins(coinA, coinB)
 	require.NoError(t, testInput.BankKeeper.MintCoins(testInput.Context, types.ModuleName, coinPair))
 	requestAddress, _ := sdk.AccAddressFromBech32(addr)
 	require.NoError(t, testInput.BankKeeper.SendCoinsFromModuleToAccount(testInput.Context, types.ModuleName, requestAddress, coinPair))
@@ -146,7 +161,6 @@ func TestCreatePool_Insufficient_Funds_ReSubmit(t *testing.T) {
 	response1, err1 := keeper.NewMsgServerImpl(*testInput.MarketKeeper).CreatePool(sdk.WrapSDKContext(testInput.Context), &p1)
 	require.NoError(t, err)
 	require.Error(t, err1)
-	//require.(t, err1, "insufficient funds")
 	require.Contains(t, p.GetCreator(), response.String())
 	require.NotContains(t, p.GetCreator(), response1.String())
 
@@ -154,7 +168,7 @@ func TestCreatePool_Insufficient_Funds_ReSubmit(t *testing.T) {
 
 /*
 func TestCreatePool_Invalid_Coins(t *testing.T) {
-	testInput, ctx := keepertest.CreateTestEnvironment(t)
+	testInput := keepertest.CreateTestEnvironment(t)
 
 	scenarios := []struct {
 		coinAStr      string
@@ -180,15 +194,14 @@ func TestCreatePool_Invalid_Coins(t *testing.T) {
 		}
 
 		coinPair := sdk.NewCoins(coinA, coinB)
-		require.NoError(t, testInput.BankKeeper.MintCoins(ctx, types.ModuleName, coinPair))
+		require.NoError(t, testInput.BankKeeper.MintCoins(testInput.Context, types.ModuleName, coinPair))
 		requestAddress, _ := sdk.AccAddressFromBech32(addr)
-		require.NoError(t, testInput.BankKeeper.SendCoinsFromModuleToAccount(ctx, types.ModuleName, requestAddress, coinPair))
+		require.NoError(t, testInput.BankKeeper.SendCoinsFromModuleToAccount(testInput.Context, types.ModuleName, requestAddress, coinPair))
 		var p = types.MsgCreatePool{CoinA: s.coinAStr, CoinB: s.coinBStr, Creator: addr, RateA: s.RateAstrArray, RateB: s.RateBstrArray}
-		response, err := keeper.NewMsgServerImpl(*testInput.MarketKeeper).CreatePool(sdk.WrapSDKContext(ctx), &p)
+		response, err := keeper.NewMsgServerImpl(*testInput.MarketKeeper).CreatePool(sdk.WrapSDKContext(testInput.Context), &p)
 		require.Error(t, err)
 		require.NotContains(t, p.GetCreator(), response.String())
 
 	}
 
-}
-*/
+}*/
