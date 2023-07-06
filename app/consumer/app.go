80fbcaf07a47c193287a5ce98849a2c525157655
Aaron Kutch <aaronkutch@att.net>
2023-07-06 16:50:10 -0500

    add in market module

--- app/consumer/app.go
+++ app/consumer/app.go
@@ -109,6 +109,10 @@ import (
 
 	// unnamed import of statik for swagger UI support
 	_ "github.com/cosmos/cosmos-sdk/client/docs/statik"
+
+	marketmodule "github.com/pendulum-labs/market/x/market"
+	marketmodulekeeper "github.com/pendulum-labs/market/x/market/keeper"
+	marketmoduletypes "github.com/pendulum-labs/market/x/market/types"
 )
 
 const (
@@ -147,6 +151,7 @@ var (
 		vesting.AppModuleBasic{},
 		//router.AppModuleBasic{},
 		consumer.AppModuleBasic{},
+		marketmodule.AppModuleBasic{},
 	)
 
 	// module account permissions
@@ -160,6 +165,7 @@ var (
 		consumertypes.ConsumerToSendToProviderName: nil,
 		ibctransfertypes.ModuleName:                {authtypes.Minter, authtypes.Burner},
 		govtypes.ModuleName:                        {authtypes.Burner},
+		marketmoduletypes.ModuleName:               {authtypes.Minter, authtypes.Burner, authtypes.Staking},
 	}
 )
 
@@ -210,6 +216,8 @@ type App struct { // nolint: golint
 	ScopedTransferKeeper    capabilitykeeper.ScopedKeeper
 	ScopedIBCConsumerKeeper capabilitykeeper.ScopedKeeper
 
+	MarketKeeper marketmodulekeeper.Keeper
+
 	// the module manager
 	MM *module.Manager
 
@@ -251,12 +259,23 @@ func New(
 	bApp.SetInterfaceRegistry(interfaceRegistry)
 
 	keys := sdk.NewKVStoreKeys(
-		authtypes.StoreKey, banktypes.StoreKey, stakingtypes.StoreKey,
-		minttypes.StoreKey, distrtypes.StoreKey, slashingtypes.StoreKey,
-		govtypes.StoreKey, paramstypes.StoreKey, ibchost.StoreKey, upgradetypes.StoreKey, feegrant.StoreKey,
-		evidencetypes.StoreKey, ibctransfertypes.StoreKey,
-		capabilitytypes.StoreKey, authzkeeper.StoreKey,
+		authtypes.StoreKey,
+		banktypes.StoreKey,
+		stakingtypes.StoreKey,
+		minttypes.StoreKey,
+		distrtypes.StoreKey,
+		slashingtypes.StoreKey,
+		govtypes.StoreKey,
+		paramstypes.StoreKey,
+		ibchost.StoreKey,
+		upgradetypes.StoreKey,
+		feegrant.StoreKey,
+		evidencetypes.StoreKey,
+		ibctransfertypes.StoreKey,
+		capabilitytypes.StoreKey,
+		authzkeeper.StoreKey,
 		consumertypes.StoreKey,
+		marketmoduletypes.StoreKey,
 	)
 	tkeys := sdk.NewTransientStoreKeys(paramstypes.TStoreKey)
 	memKeys := sdk.NewMemoryStoreKeys(capabilitytypes.MemStoreKey)
@@ -468,6 +487,16 @@ func New(
 
 	app.EvidenceKeeper = *evidenceKeeper
 
+	app.MarketKeeper = *marketmodulekeeper.NewKeeper(
+		appCodec,
+		keys[marketmoduletypes.StoreKey],
+		keys[marketmoduletypes.MemStoreKey],
+		app.GetSubspace(marketmoduletypes.ModuleName),
+
+		app.BankKeeper,
+	)
+	marketModule := marketmodule.NewAppModule(appCodec, app.MarketKeeper, app.AccountKeeper, app.BankKeeper)
+
 	skipGenesisInvariants := cast.ToBool(appOpts.Get(crisis.FlagSkipGenesisInvariants))
 
 	// NOTE: Any module instantiated in the module manager that is later modified
@@ -491,6 +520,7 @@ func New(
 		ibc.NewAppModule(app.IBCKeeper),
 		transferModule,
 		consumerModule,
+		marketModule,
 	)
 
 	// During begin block slashing happens after distr.BeginBlocker so that
@@ -518,6 +548,7 @@ func New(
 		vestingtypes.ModuleName,
 		ibctransfertypes.ModuleName,
 		ibchost.ModuleName,
+		marketmoduletypes.ModuleName,
 		consumertypes.ModuleName,
 	)
 	app.MM.SetOrderEndBlockers(
@@ -538,6 +569,7 @@ func New(
 		vestingtypes.ModuleName,
 		ibctransfertypes.ModuleName,
 		ibchost.ModuleName,
+		marketmoduletypes.ModuleName,
 		consumertypes.ModuleName,
 	)
 
@@ -565,6 +597,7 @@ func New(
 		vestingtypes.ModuleName,
 		ibchost.ModuleName,
 		ibctransfertypes.ModuleName,
+		marketmoduletypes.ModuleName,
 		consumertypes.ModuleName,
 	)
 
@@ -592,6 +625,7 @@ func New(
 		evidence.NewAppModule(app.EvidenceKeeper), ibc.NewAppModule(app.IBCKeeper),
 		authzmodule.NewAppModule(appCodec, app.AuthzKeeper, app.AccountKeeper, app.BankKeeper, app.interfaceRegistry),
 		transferModule,
+		marketModule,
 	)
 
 	app.sm.RegisterStoreDecoders()
@@ -900,6 +934,7 @@ func initParamsKeeper(appCodec codec.BinaryCodec, legacyAmino *codec.LegacyAmino
 	paramsKeeper.Subspace(ibctransfertypes.ModuleName)
 	paramsKeeper.Subspace(ibchost.ModuleName)
 	paramsKeeper.Subspace(consumertypes.ModuleName)
+	paramsKeeper.Subspace(marketmoduletypes.ModuleName)
 
 	return paramsKeeper
 }
